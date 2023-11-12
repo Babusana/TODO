834f81921b4f3712660697e3eae7233ea99a20a7
Babusana <babusanamahanta71@gmail.com>
2023-11-12 15:40:43 +0530

    fixed

--- app/page.js
+++ app/page.js
@@ -29,8 +29,12 @@ const Page = () => {
 			return (
 				<li className="flex items-center justify-between mb-8" key={i}>
 					<div className="flex justify-between mb-5 w-2/3 items-center">
-						<h5 className="text-2xl font-semibold ml-5 uppercase">{t.title}</h5>
-						<h6 className="text-lg font-medium">{t.desc}</h6>
+						<div className="titlec flex flex-col justify-center items-center ml-24">
+							<h5 className="text-2xl font-semibold   title">{t.title}</h5>
+						</div>
+						<div className="descc flex flex-col justify-center items-center">
+							<h6 className="text-lg font-medium desc">{t.desc}</h6>
+						</div>
 					</div>
 					<button
 						className="bg-red-400 text-white px-4 py-2 rounded font-bold"
@@ -45,27 +49,28 @@ const Page = () => {
 	
   return (
       <>
-          <h1 className="bg-black text-white p-4 text-4xl font-bold text-center">My Todo List</h1>
-		  <form onSubmit={submitHandler}>
+          <h1 className="bg-black text-white p-4 text-4xl font-bold text-center" contentEditable="true">My Todo List</h1>
+		  <form onSubmit={submitHandler} className= "container">
 			  <input type="text"
-			  className="text-xl border-zinc-800 border-2 m-8 px-3 py-2 rounded"
+			  className="text-xl border-zinc-800 border-2 m-8 px-3 py-2 rounded field"
 			  placeholder="Enter Title here"
 			  value={title}
 			  onChange={(e) =>{
 				  settitle(e.target.value)
 			  }}/>
 			  <input type="text"
-					 className="text-xl border-zinc-800 border-2 m-8 px-3 py-2 rounded"
+					 className="text-xl border-zinc-800 border-2 m-8 px-3 py-2 rounded field"
 					 placeholder="Enter Description here"
 			  value={desc}
 			  onChange={(e) =>{
 				  setdesc(e.target.value)
 			  }}/>
-			  <button className="bg-black text-white  px-3 py-2 text-2xl rounded font-bold">Add Task</button>
+			  <button className="bg-black text-white  px-3 py-2 text-2xl rounded font-bold" id= "btn">Add Task</button>
 		  </form>
 		  <hr/>
 		  <div className="p-8 bg-slate-200">
 			  <ul>
+
 				  {renderTask}
 			  </ul>
 		  </div>
